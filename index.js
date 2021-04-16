a58b1a96c19362d4864fed7f439ae5d56c94a9ed
Asif2535 <aiqbal517@gmail.com>
2021-04-16 15:23:52 +0600

    connected with database

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const MongoClient = require('mongodb').MongoClient;
+const app = express();
+const ObjectID = require('mongodb').ObjectID;
+const cors = require('cors');
+const bodyParser = require('body-parser');
+require('dotenv').config()
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wp8tr.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+
+
+app.get('/', (req, res) => {
+    res.send('welcome to event photography api')
+})
+
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+client.connect(err => {
+    const foodCollection = client.db(process.env.DB_NAME).collection("admins");
+    console.log('db connected');
+});
+
+
+
+
+
+
+app.listen(port, () => {
+    console.log(`Example app listening port at http://localhost:${port}`)
+})
